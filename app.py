2b52b52625e61d2a39f5b92ae846b7242d28b3dd
muthiahhasna21 <muthiahhasna12@gmail.com>
2025-05-29 21:59:15 +0700

    commit 3, memperbarui isi landing page berisi tampilan profil dan daftar film populer

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
-import requests
+import requests 
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    url = "https://imdb236.p.rapidapi.com/api/imdb/most-popular-movies"
+    headers = {
+        "x-rapidapi-key": "fe1fafe432msh517b2ce221ff25ap135b06jsne7da14e55876",
+        "x-rapidapi-host": "imdb236.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+    all_movies = response.json()  # langsung list
+
+    return render_template('home.html', movies=all_movies)
 
 @app.route('/movies')
 def movies():
@@ -21,6 +31,23 @@ def movies():
     # Pastikan data yang dipassing ke template adalah list film
     movies = response.json() # tergantung struktur JSON response
     return render_template('index.html', movies=movies)
+
+@app.route('/genres')
+def genres():
+    url = "https://imdb236.p.rapidapi.com/api/imdb/genres"
+    headers = {
+        "x-rapidapi-key": "fe1fafe432msh517b2ce221ff25ap135b06jsne7da14e55876",
+        "x-rapidapi-host": "imdb236.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    genre_list = response.json()  # pastikan ini sesuai struktur JSON API kamu
+
+    return render_template("genres.html", genres=genre_list)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
